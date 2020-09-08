570797b380fe719343e5440916238f3a18992071
Zoltan Szeman <zoltan1szeman@gmail.com>
2020-09-08 16:08:34 +0200

    Corrected digit number input

--- pi_nthdigit.py
+++ pi_nthdigit.py
@@ -2,14 +2,16 @@
 # 2020-09-08
 # Task: Print pi to the nth digit
 
+from mpmath import mp
 from math import pi
 
 while True:
     try:
-        nth_digit = int(input('Enter, what digit precision '
+        digit_no = int(input('Enter, how many digits of '
             'should pi be printed: '))
         break
     except ValueError:
         print('\nPlease enter a valid whole number!\n')
 
-print(pi)
+mp.dps = digit_no    # set number of digits
+print(f'Pi to the {digit_no}th digit is {mp.pi}')
